23b733e3492a0876b983d7d5bef8b606b3387fd0
Anish Zala <azala@affirma.com>
2023-01-30 16:36:56 +0530

    Add project files.

---
+++ BlobManagedIdentity/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+
+string blobURI = "https://anishstorage786.blob.core.windows.net/anishcontainer/AZ-900.txt";
+string filePath = "C:\\ImagesFolder\\anishblob.txt";
+
+TokenCredential tokenCredential = new DefaultAzureCredential();
+
+BlobClient blobClient = new BlobClient(new Uri(blobURI), tokenCredential);
+
+await blobClient.DownloadToAsync(filePath);
+
+Console.WriteLine("The blob is downloaded");
+
+
